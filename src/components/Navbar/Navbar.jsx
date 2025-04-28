60cf2219c443c635dda56c07d7c22fc24b9d681b
Benedict Notoane <benni@Benedicts-MacBook-Pro.local>
2025-04-28 11:33:11 +0200

    created signup page and redirected to main page after signup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,45 @@
-import { NavLink } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { auth } from "../../firebaseConfig";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/sign-in");
+      console.log("User signed out successfully");
+    } catch (error) {
+      console.log("Error signing out:", error.message);
+    }
+  };
+
   return (
     <nav className={styles.navbar}>
       <NavLink to="/">Recipes</NavLink>
       <NavLink to="add-recipe">Add Recipes</NavLink>
-      <button className={styles.signOutButton}>Sign Out</button>
+      {isLoggedIn && (
+        <button onClick={handleSignOut} className={styles.signOutButton}>
+          Sign Out
+        </button>
+      )}
     </nav>
   );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,152 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebaseConfig";
 import styles from "./SignUp.module.css";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  // Declaring state variables
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    terms: false,
+  });
+
+  const [error, setError] = useState(null);
+
+  // For redirecting
+  const navigate = useNavigate();
+
+  // Retrieving input values
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: checked,
+    }));
+  };
+
+  const handleSignUp = async (e, email, password) => {
+    e.preventDefault();
+    setError(null);
+    if (formData.password !== formData.confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      console.log("User signed up successfully:", user);
+
+      navigate("/");
+
+      // Reset form
+      setFormData({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        terms: false,
+      });
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className={styles.signUpContainer}>
+      <form
+        className={styles.signUpForm}
+        onSubmit={(e) => handleSignUp(e, formData.email, formData.password)}
+      >
+        <h1>Sign Up 🌮</h1>
+        <div className={styles.inputGroup}>
+          <label htmlFor="firstname">First name</label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            onChange={handleChange}
+            value={formData.firstname}
+          />
+        </div>
+        {/* ------------- */}
+        <div className={styles.inputGroup}>
+          <label htmlFor="lastname">Last name</label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            onChange={handleChange}
+            value={formData.lastname}
+          />
+        </div>
+        {/* ------------- */}
+        <div className={styles.inputGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            value={formData.email}
+          />
+        </div>
+        {/* ------------- */}
+        <div className={styles.inputGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="confirm-password">Confirm password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirm-password"
+            onChange={handleChange}
+            value={formData.confirmPassword}
+          />
+        </div>
+        {/* ------------- */}
+        <div className={styles.term}>
+          <input
+            type="checkbox"
+            name="terms"
+            id="terms"
+            onChange={handleCheckboxChange}
+            checked={formData.terms}
+          />
+          <label htmlFor="terms">I agree to the terms and conditions</label>
+        </div>
+        {error && <p>{error}</p>}
+        {/* ------------- */}
+        <button className={styles.submitButton}>Sign Up</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
